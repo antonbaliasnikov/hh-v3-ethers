2e584db8342fa6cfb3970acfa6608d2c401a9bd4
Anton Baliasnikov <aba@matterlabs.dev>
2025-09-25 14:16:31 +0200

    chore: add hardhat v3 ethers template

---
+++ hardhat.config.ts
@@ -0,0 +1,52 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  ignition: {
+    requiredConfirmations: 1,
+  },
+  networks: {
+    [process.env.NETWORK_NAME || "ZKsyncOS"]: {
+      type: "http",
+      chainType: "generic",
+      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 270,
+      url: process.env.RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+};
+
+export default config;
